c3ed408b9e51141b689f6c3032e79788e49cec42
Kuan <kuan@hey.com>
2022-03-20 17:18:30 +0800

    Some css sprinkle

--- src/view/Home.svelte
+++ src/view/Home.svelte
@@ -4,13 +4,19 @@
   const {venues, goToVenues} = HomeStores;
 </script>
 
-<h1>Home</h1>
+<section class="p-8 bg-blue-300">
+	<h1 class="text-2xl">Access Audit</h1>
+	<h6 class="text-base">TaraKita</h6>
+</section>
 
-<div>
-  <h2>Venues</h2>
+<section class="p-4 container">
   <ul>
     {#each $venues as venue, index}
-    	<button on:click={$goToVenues[index]}>{venue}</button>
+      <li class="m-4">
+      	<button
+				  class="bg-blue-500 hover:bg-blue-dark text-white font-bold py-2 px-6 w-full rounded"
+          on:click={$goToVenues[index]}>{venue}</button>
+      </li>
     {/each}
   </ul>
-</div>
+</section>
--- src/view/OptionList.svelte
+++ src/view/OptionList.svelte
@@ -4,19 +4,22 @@
   const {ancestors, options, goToOptions} = OptionStores;
 </script>
 
-<div>
+<section class="p-2 bg-blue-300">
   {#each $ancestors as ancestor}
   	<button>{ancestor}</button>
   {/each}
-</div>
+</section>
 
-<div>
-  <h2>List goes here</h2>
+<section class="p-4 container">
   <ul>
     {#each $options as option, index}
-    	<button on:click={$goToOptions[index]}>{option}</button>
+    <li class="m-4">
+      	<button
+				  class="bg-blue-500 hover:bg-blue-dark text-white font-bold py-2 px-6 w-full rounded"
+        	on:click={$goToOptions[index]}>{option}</button>
+    </li>
     {/each}
   </ul>
-</div>
+</section>
 
 
