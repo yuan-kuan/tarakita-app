613cdd19886ebc205745232d599a0479595d08d8
Kuan <kuan@hey.com>
2022-03-21 21:24:45 +0800

    DIY form handling for register page

---
+++ src/view/BigCheck.svelte
@@ -0,0 +1,31 @@
+<script>
+  export let main = false;
+	export let value;
+	export let title;
+</script>
+
+
+<div class="mb-4">
+  <label class="flex justify-start items-start">
+    {#if main}
+    <div class="select-none text-lg font-bold">{title}</div>
+    <div class="bg-white border-2 rounded-full border-gray-400 w-10 h-10 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-blue-500">
+      <input type="checkbox" class="opacity-0 absolute" bind:checked={value}>
+      <svg class="fill-current hidden w-7 h-7 text-green-500 pointer-events-none" viewBox="0 0 20 20"><path d="M0 11l2-2 5 5L18 3l2 2L7 18z"/></svg>
+    </div>
+    {:else}
+    <div class="bg-white border-2 rounded-full border-gray-400 w-8 h-8 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-blue-500">
+      <input type="checkbox" class="opacity-0 absolute" bind:checked={value}>
+      <svg class="fill-current hidden w-6 h-6 text-green-500 pointer-events-none" viewBox="0 0 20 20"><path d="M0 11l2-2 5 5L18 3l2 2L7 18z"/></svg>
+    </div>
+    <div class="select-none text-lg">{title}</div>
+    {/if}
+  </label>
+</div>
+
+<style>
+  input:checked + svg {
+  	display: block;
+  }
+</style>
+
--- src/view/Register.svelte
+++ src/view/Register.svelte
@@ -2,30 +2,68 @@
   import { createForm } from "svelte-forms-lib";
   import { UserStores } from 'app/stores';
 
+  import BigCheck from 'view/BigCheck.svelte';
+
 	const { performRegister } = UserStores;
 
-  const { form, handleChange, handleSubmit } = createForm({
-    initialValues: {
-      name: "",
-    },
-    onSubmit: values => {
-      $performRegister(values);
-    }
-  });
+	let name = '';
+	let oku = false;
+	let blind = false;
+	let deaf = false;
+	let lowMobile = false;
+	let others = false;
+
+	const handleSubmit = (event) => {
+	  event.preventDefault();
+	  let forms = [];
+	  if (blind) forms.push('blind');
+	  if (deaf) forms.push('deaf');
+	  if (lowMobile) forms.push('lowMobile');
+	  if (others) forms.push('others');
+
+		let finalOku = forms.length > 0;
+	  let data = {
+			name, oku: finalOku, forms
+	  };
+
+		$performRegister(data);
+	}
+
 </script>
 
 <header>
   <p>Logo Goes Here</p>
 </header>
 
-<form on:submit={handleSubmit}>
-  <label for="name">name</label>
-  <input
-    id="name"
-    name="name"
-    on:change={handleChange}
-    bind:value={$form.name}
-  />
+<form
+  class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+  on:submit={handleSubmit}>
+  
+  <div class="mb-4">
+    <label class = "block text-gray-700 text-lg font-bold mb-2" for="name">
+      Name
+     </label>
+    <input
+      class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      bind:value={name}
+    />
+  </div>
 
-  <button type="submit">Start</button>
+  <div class="mb-4">
+		<BigCheck bind:value={oku} title="OKU" main={true} />
+	</div>
+
+	{#if oku}
+		<div>
+    	<BigCheck bind:value={blind} title="Blind / Low vision" />
+  		<BigCheck bind:value={deaf} title="Deaf / Audio Barriers" />
+  		<BigCheck bind:value={lowMobile} title="Wheelchair / Scooter User / Low Mobility" />
+  		<BigCheck bind:value={others} title="Others" />
+		</div>
+	{/if}
+
+  <button
+  	class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+  	type="submit">Start</button>
 </form>
+
