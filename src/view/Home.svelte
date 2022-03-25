ba2744755f8472d3e3439fe181f9fd8380a842cd
Kuan <kuan@hey.com>
2022-03-25 18:34:33 +0800

    style venue page

--- src/view/Home.svelte
+++ src/view/Home.svelte
@@ -4,26 +4,39 @@
   const { venues, goToVenues, downloadingQuestion } = HomeStores;
 </script>
 
-<section class="bg-blue-300 p-8">
-  <h1 class="text-2xl">Access Audit</h1>
-  <h6 class="text-base">TaraKita</h6>
-</section>
+<div class="flex min-h-screen flex-col">
+  <section class="bg-primary p-8">
+    <h1 class="text-2xl text-white">Where are you?</h1>
+  </section>
 
-<section class="container p-4">
-  <ul>
+  <section class="flex grow flex-col items-center pt-20">
     {#each $venues as venue, index}
-      <li class="m-4">
-        <button
-          class="hover:bg-blue-dark w-full rounded bg-blue-500 py-2 px-6 font-bold text-white"
-          on:click={$goToVenues[index]}>{venue}</button
-        >
-      </li>
+      <button class="topic-btn" on:click={$goToVenues[index]}>{venue}</button>
     {/each}
 
     {#if $downloadingQuestion}
-      <li>
-        <span class="w-full px-6"> Downloading new questions... </span>
-      </li>
+      <span class="px-6"> Downloading new questions... </span>
     {/if}
-  </ul>
-</section>
+  </section>
+</div>
+
+<div
+  class="fixed bottom-14 left-1/2 mx-auto inline-flex w-5/6  -translate-x-1/2 transform items-center justify-between "
+>
+  <button class="focus:outline-none rounded-full bg-black font-bold text-white">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-14 w-14"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+        clip-rule="evenodd"
+      />
+    </svg>
+  </button>
+
+  <span />
+</div>
--- src/view/Tailwindcss.svelte
+++ src/view/Tailwindcss.svelte
@@ -2,4 +2,8 @@
   @tailwind base;
   @tailwind components;
   @tailwind utilities;
+
+  .topic-btn {
+    @apply w-2/3 rounded-full bg-primary py-2 px-6 font-bold text-white;
+  }
 </style>
